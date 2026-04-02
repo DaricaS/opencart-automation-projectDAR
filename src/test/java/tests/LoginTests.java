26391254520c836ae50e1919521d9c26b2a5363b
DaricaS <daricastamkova11@gmail.com>
2026-04-02 19:03:58 +0200

    .

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,12 +1,46 @@
 package tests;
 
+import base.BaseTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
 
-public class LoginTests {
+public class LoginTests extends BaseTest {
 
-    @Test
-    public void loginTest() {
-        System.out.println("Login Test Running");
+    @Test(groups = "smoke")
+    public void testValidLogin() {
+
+        HomePage homePage = new HomePage(driver);
+        homePage.navigateToLogin();
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("test@test.com", "Password123");
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("account"));
     }
 
+    @Test(groups = "regression")
+    public void testInvalidEmail() {
+
+        HomePage homePage = new HomePage(driver);
+        homePage.navigateToLogin();
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("invalid@test.com", "Password123");
+
+        Assert.assertTrue(driver.getPageSource().contains("Warning"));
+    }
+
+    @Test(groups = "regression")
+    public void testInvalidPassword() {
+
+        HomePage homePage = new HomePage(driver);
+        homePage.navigateToLogin();
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("test@test.com", "wrongpassword");
+
+        Assert.assertTrue(driver.getPageSource().contains("Warning"));
+    }
 }
