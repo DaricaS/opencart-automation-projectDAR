64a0fc82057b9bcb4f390a8f97e62318a4ad8574
DaricaS <daricastamkova11@gmail.com>
2026-04-02 19:03:44 +0200

    .

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -1,12 +1,42 @@
 package tests;
 
+import base.BaseTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.SearchPage;
 
-public class SearchTests {
+public class SearchTests extends BaseTest {
 
-    @Test
-    public void searchTest() {
-        System.out.println("Search Test Running");
+    @Test(groups = "smoke")
+    public void testSearchValidProduct() {
+
+        HomePage homePage = new HomePage(driver);
+        homePage.searchProduct("MacBook");
+
+        SearchPage searchPage = new SearchPage(driver);
+
+        Assert.assertTrue(searchPage.getSearchResultsCount() > 0);
     }
 
+    @Test(groups = "regression")
+    public void testSearchInvalidProduct() {
+
+        HomePage homePage = new HomePage(driver);
+        homePage.searchProduct("XYZ12345");
+
+        SearchPage searchPage = new SearchPage(driver);
+
+        Assert.assertTrue(searchPage.isNoResultsMessageDisplayed());
+    }
+
+    @Test(groups = "regression")
+    public void testSearchEmptyQuery() {
+
+        HomePage homePage = new HomePage(driver);
+        homePage.searchProduct("");
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("search"));
+    }
 }
+
