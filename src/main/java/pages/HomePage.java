f116107a9720e3f78253404cd0b79470cf1a388f
DaricaS <daricastamkova11@gmail.com>
2026-04-02 21:55:52 +0200

    .

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -2,39 +2,41 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HomePage {
 
     WebDriver driver;
+    WebDriverWait wait;
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-    // Locators
+    By myAccount = By.xpath("//a[@title='My Account']");
+    By login = By.linkText("Login");
+    By register = By.linkText("Register");
     By searchBox = By.name("search");
-    By searchButton = By.cssSelector("button.btn-default");
-    By myAccountLink = By.linkText("My Account");
-    By registerLink = By.linkText("Register");
-    By loginLink = By.linkText("Login");
-
-    // Methods
-    public void searchProduct(String productName) {
-        driver.findElement(searchBox).sendKeys(productName);
-        driver.findElement(searchButton).click();
-    }
+    By searchButton = By.cssSelector("button.btn.btn-default.btn-lg");
 
     public void clickMyAccount() {
-        driver.findElement(myAccountLink).click();
+        wait.until(ExpectedConditions.elementToBeClickable(myAccount)).click();
     }
 
-    public void navigateToRegister() {
-        driver.findElement(myAccountLink).click();
-        driver.findElement(registerLink).click();
+    public void clickLogin() {
+        wait.until(ExpectedConditions.elementToBeClickable(login)).click();
     }
 
-    public void navigateToLogin() {
-        driver.findElement(myAccountLink).click();
-        driver.findElement(loginLink).click();
+    public void clickRegister() {
+        wait.until(ExpectedConditions.elementToBeClickable(register)).click();
+    }
+
+    public void searchProduct(String product) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(searchBox)).sendKeys(product);
+        driver.findElement(searchButton).click();
     }
 }
