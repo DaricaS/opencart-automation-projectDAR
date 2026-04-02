0dcdad8aaa81fe7d93d7ad962bc55a92295cb7ae
DaricaS <daricastamkova11@gmail.com>
2026-04-02 21:56:05 +0200

    .

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -2,9 +2,6 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-import java.util.List;
 
 public class CartPage {
 
@@ -14,18 +11,9 @@ public class CartPage {
         this.driver = driver;
     }
 
-    // Locators
-    By cartItems = By.cssSelector(".table-responsive tbody tr");
-    By removeButtons = By.cssSelector("button[data-original-title='Remove']");
+    By cart = By.id("cart");
 
-    // Methods
-    public int getCartItemsCount() {
-        List<WebElement> items = driver.findElements(cartItems);
-        return items.size();
-    }
-
-    public void removeItemByIndex(int index) {
-        List<WebElement> buttons = driver.findElements(removeButtons);
-        buttons.get(index).click();
+    public void openCart() {
+        driver.findElement(cart).click();
     }
 }
