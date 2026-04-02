f1acc0524d73066ecfc465bd6694b973f99db85d
DaricaS <daricastamkova11@gmail.com>
2026-04-02 18:57:43 +0200

    .

--- src/main/java/pages/RegisterPage.java
+++ src/main/java/pages/RegisterPage.java
@@ -1,4 +1,37 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class RegisterPage {
+
+    WebDriver driver;
+
+    public RegisterPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Locators
+    By firstNameField = By.id("input-firstname");
+    By lastNameField = By.id("input-lastname");
+    By emailField = By.id("input-email");
+    By telephoneField = By.id("input-telephone");
+    By passwordField = By.id("input-password");
+    By confirmPasswordField = By.id("input-confirm");
+    By privacyPolicyCheckbox = By.name("agree");
+    By continueButton = By.cssSelector("input.btn-primary");
+
+    // Method
+    public void registerUser(String firstName, String lastName, String email, String phone, String password) {
+
+        driver.findElement(firstNameField).sendKeys(firstName);
+        driver.findElement(lastNameField).sendKeys(lastName);
+        driver.findElement(emailField).sendKeys(email);
+        driver.findElement(telephoneField).sendKeys(phone);
+        driver.findElement(passwordField).sendKeys(password);
+        driver.findElement(confirmPasswordField).sendKeys(password);
+        driver.findElement(privacyPolicyCheckbox).click();
+        driver.findElement(continueButton).click();
+    }
 }
+
--- src/main/java/pages/SearchPage.java
+++ src/main/java/pages/SearchPage.java
@@ -1,4 +1,36 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 public class SearchPage {
+
+    WebDriver driver;
+
+    public SearchPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Locators
+    By searchResults = By.cssSelector(".product-thumb");
+    By noResultsMessage = By.xpath("//p[contains(text(),'no product')]");
+
+    // Methods
+    public int getSearchResultsCount() {
+        List<WebElement> results = driver.findElements(searchResults);
+        return results.size();
+    }
+
+    public boolean isNoResultsMessageDisplayed() {
+        return driver.findElement(noResultsMessage).isDisplayed();
+    }
+
+    public void clickProductByIndex(int index) {
+        List<WebElement> results = driver.findElements(searchResults);
+        results.get(index).click();
+    }
 }
+
