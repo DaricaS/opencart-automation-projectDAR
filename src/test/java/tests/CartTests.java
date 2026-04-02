fc60e5b8c12691e2639b9f070251fb12075733f4
DaricaS <daricastamkova11@gmail.com>
2026-04-02 19:05:25 +0200

    .

--- src/test/java/tests/CartTests.java
+++ src/test/java/tests/CartTests.java
@@ -1,12 +1,45 @@
 package tests;
 
+import base.BaseTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.*;
 
-public class CartTests {
+public class CartTests extends BaseTest {
 
-    @Test
-    public void cartTest() {
-        System.out.println("Cart Test Running");
+    @Test(groups = "smoke")
+    public void testAddToCart() {
+
+        HomePage homePage = new HomePage(driver);
+        homePage.searchProduct("MacBook");
+
+        SearchPage searchPage = new SearchPage(driver);
+        searchPage.clickProductByIndex(0);
+
+        ProductPage productPage = new ProductPage(driver);
+        productPage.addToCart();
+
+        Assert.assertTrue(productPage.isSuccessMessageDisplayed());
     }
 
+    @Test(groups = "regression")
+    public void testRemoveFromCart() {
+
+        HomePage homePage = new HomePage(driver);
+        homePage.searchProduct("MacBook");
+
+        SearchPage searchPage = new SearchPage(driver);
+        searchPage.clickProductByIndex(0);
+
+        ProductPage productPage = new ProductPage(driver);
+        productPage.addToCart();
+
+        driver.get("https://demo.opencart.com/index.php?route=checkout/cart");
+
+        CartPage cartPage = new CartPage(driver);
+        cartPage.removeItemByIndex(0);
+
+        Assert.assertTrue(cartPage.getCartItemsCount() == 0);
+    }
 }
+
