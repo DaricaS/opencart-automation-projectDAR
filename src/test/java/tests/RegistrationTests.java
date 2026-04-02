5fe6d0303262188a1f1d7f2665e5cf83779a8bb7
DaricaS <daricastamkova11@gmail.com>
2026-04-02 19:01:08 +0200

    .

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -1,12 +1,48 @@
 package tests;
 
+import base.BaseTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.RegisterPage;
 
-public class RegistrationTests {
+public class RegistrationTests extends BaseTest {
 
-    @Test
-    public void registrationTest() {
-        System.out.println("Registration Test Running");
+    @Test(groups = "smoke")
+    public void testSuccessfulRegistration() {
+
+        HomePage homePage = new HomePage(driver);
+        homePage.navigateToRegister();
+
+        RegisterPage registerPage = new RegisterPage(driver);
+
+        registerPage.registerUser(
+                "Test",
+                "User",
+                "test123@test.com",
+                "123456789",
+                "Password123"
+        );
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("success"));
     }
 
+    @Test(groups = "regression")
+    public void testRegistrationWithExistingEmail() {
+
+        HomePage homePage = new HomePage(driver);
+        homePage.navigateToRegister();
+
+        RegisterPage registerPage = new RegisterPage(driver);
+
+        registerPage.registerUser(
+                "Test",
+                "User",
+                "test@test.com",
+                "123456789",
+                "Password123"
+        );
+
+        Assert.assertTrue(driver.getPageSource().contains("already"));
+    }
 }
