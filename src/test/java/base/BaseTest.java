3200218594b1506083f9fb1ee07b3878f7eebd74
DaricaS <daricastamkova11@gmail.com>
2026-04-02 22:14:57 +0200

    .

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,6 +2,7 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -14,19 +15,20 @@ public class BaseTest {
     @BeforeMethod
     public void setUp() {
 
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        driver.get("https://demo.opencart.com");
-
+        driver.get("https://demo.opencart.com/");
     }
 
     @AfterMethod
     public void tearDown() {
 
         driver.quit();
-
     }
-
 }
+
